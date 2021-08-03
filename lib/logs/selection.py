0ed86eb12e31687a3cc56ace85fcf09308b218cd
Isabelle Ondracek <ikondra546@gmail.com>
2021-08-03 12:30:53 -0400

    wrote function to slice audio or video file excerpts (haven't tested)

--- lib/logs/selection.py
+++ lib/logs/selection.py
@@ -1,6 +1,8 @@
 # Functions related to selecting and filtering analysis dataframes
 
 import pandas as pd
+import ffmpy
+
 
 def filter_df(df, start_time, stop_time, players, events):
     new = df
@@ -14,3 +16,23 @@ def filter_df(df, start_time, stop_time, players, events):
         new = new[new['event'].isin(events)]
     return new
 
+
+# start/length time format: HH:MM:SS.xxx
+def slice_excerpt(input_filepath, start_time, length, label):
+    # get the name of the file minus the path
+    input_filename = input_filepath.split('/').pop()
+    print(input_filename)
+
+    # get the name of the file minus the extension
+    output = input_filename.split('.')
+    # create a new file path with a descriptive label
+    output_filepath = output[0] + '_' + label + '.' + output[1]
+    print(output_filepath)
+
+    # constructs the ffmpeg command
+    ff = ffmpy.FFmpeg(
+        inputs={input_filepath: '-ss ' + start_time},
+        outputs={output_filepath: '-t ' + length + ' -c copy'}
+    )
+    print(ff.cmd)
+    # ff.run()
--- lib/logs/testing.py
+++ lib/logs/testing.py
@@ -7,7 +7,9 @@ def main():
     filename = '../../data/df/production.csv'
     dataframe = pd.read_csv(filename)
 
-    print(dataframe)
+    # print(dataframe)
+    file_excerpt = "../../data/minecraft_replay_recordings/2021_07_07_Workshop_1.mcpr"
+    selection.slice_excerpt(file_excerpt, '00:00:04', '00:01:03', 'test')
 
 
 if __name__ == '__main__':
