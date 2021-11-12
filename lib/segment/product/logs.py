055556ed3b7105863d67d33627564850d5549822
Chris Proctor <chris@chrisproctor.net>
2021-11-12 13:44:52 -0500

    Adding trace segment code

--- lib/segment/product/logs.py
+++ lib/segment/product/logs.py
@@ -1,23 +1,44 @@
+from pathlib import Path
 import pandas as pd
 from datetime import timedelta
 from segment.product.base import SegmentProduct
 
 class SegmentLogs(SegmentProduct):
     """A log file in .csv format.
+    When `use_cache` is true, will use a cached subset of the main log file when possible.
     """
-    # TEMP for efficiency while testing, you can cache a smaller df
-    #main_log_file = "log_slice_temp.csv"
+    optional_params = [
+        "use_cache",
+    ]
     main_log_file = "data/df/production.csv"
 
     def get_segment_data(self):
         """Returns a dataframe 
         """
-        df = pd.read_csv(self.main_log_file, index_col="timestamp", parse_dates=["timestamp"])
+        if self.params.get("use_cache") and self.get_cached_log_path().exists():
+            datapath = self.get_cached_log_path()
+        else:
+            datapath = self.main_log_file
+        df = pd.read_csv(datapath, index_col="timestamp", parse_dates=["timestamp"])
         df = df.sort_index()
-        start = self.segment_params['start']
-        end = start + timedelta(seconds=self.segment_params['duration'])
-        return df.loc[start:end]
+        start, end = self.get_start_end_times()
+        df = df.loc[start:end]
+        if self.params.get("use_cache") and not self.get_cached_log_path().exists():
+            self.get_cached_log_path().parent.mkdir(parents=True, exist_ok=True)
+            df.to_csv(self.get_cached_log_path())
+        return df
 
     def export(self):
         self.get_segment_data().to_csv(self.export_filename())
+
+    def get_cached_log_path(self):
+        start, end = self.get_start_end_times()
+        return Path("data/cache") / f"{start}-{end}.csv"
+
+    def get_start_end_times(self):
+        "Returns (start, end) times"
+        start = self.segment_params['start']
+        end = start + timedelta(seconds=self.segment_params['duration'])
+        return start, end
+        
 
--- lib/segment/product/trace.py
+++ lib/segment/product/trace.py
@@ -1,5 +1,72 @@
-from segment.product.base import SegmentProduct
+from segment.product.logs import SegmentLogs
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
-class SegmentTrace(SegmentProduct):
-    "Produces a trace, or a plan view illustration of where characters have moved"
+class SegmentTrace(SegmentLogs):
+    """Produces a heatmap of character locations
+    """
+    optional_params = [
+        "use_cache",
+        "plot_filename",
+        "players",
+        "granularity",
+        "alpha",
+        "size",
+        "palette",
+        "diachronic"
+    ]
+    default_granularity = "1s"
+    default_alpha = 0.1
+    default_size=50
+    default_diachronic_palette="flare"
+    default_heatmap_palette="hls"
 
+    def export(self):
+        df = self.get_segment_data()
+        df = df[["player", "location_x", "location_z"]]
+        if self.params.get("players"):
+            df = df[df.player.isin(self.params['players'])]
+        granularity = self.params.get("granularity", self.default_granularity)
+        df = df.resample(granularity).first().dropna()
+        df.to_csv(self.export_filename())
+        if self.params.get('plot_filename'):
+            sns.set_theme(style="white")
+            if self.params.get("diachronic"):
+                self.plot_diachronic(df)
+            else:
+                self.plot_heatmap(df)
+            plt.savefig(self.export_filename('plot_filename'), bbox_inches="tight")
+        
+    def plot_diachronic(self, df):
+        df = df.reset_index()
+        palette = self.params.get("palette", self.default_diachronic_palette)
+        ax = sns.relplot(
+            data=df,
+            col="player",
+            hue="timestamp",
+            x="location_x",
+            y="location_z",
+            alpha=self.params.get("alpha", self.default_alpha),
+            edgecolor=None,
+            legend=False,
+            palette=palette,
+            s=self.params.get("size", self.default_size),
+        )
+        norm = plt.Normalize(df.timestamp.min().value, df.timestamp.max().value)
+        sm = plt.cm.ScalarMappable(cmap=palette, norm=norm)
+        sm.set_array([df.index.min(), df.index.max()])
+        cbar = ax.figure.colorbar(sm)
+        cbar.ax.set_yticklabels(pd.to_datetime(cbar.get_ticks()).strftime(date_format='%H:%M'))
+            
+    def plot_heatmap(self, df):
+        return sns.relplot(
+            data=df,
+            hue="player",
+            x="location_x",
+            y="location_z",
+            alpha=self.params.get("alpha", self.default_alpha),
+            palette=self.params.get("palette", self.default_heatmap_palette),
+            edgecolor=None,
+            s=self.params.get("size", self.default_size),
+        )
