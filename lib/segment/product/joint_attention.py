e8885df5076da1db8b4ffcb29133312bf14cec33
Chris Proctor <chris@chrisproctor.net>
2021-11-12 15:32:07 -0500

    Adding default JVA

--- lib/segment/product/joint_attention.py
+++ lib/segment/product/joint_attention.py
@@ -24,6 +24,7 @@ class SegmentJointAttention(SegmentLogs):
         "measure",
     ]
     optional_params = [
+        "use_cache",
         "lookback",
         "plot_filename",
         "distance_threshold",
@@ -31,22 +32,17 @@ class SegmentJointAttention(SegmentLogs):
         "plot_title",
     ]
     default_plot_title = "Joint Visual Attention"
+    default_distance_threshold = 10
+    default_window_seconds = 2
 
-    def get_location_gaze(self, lookback=0, lookahead=0):
-        """Returns a location gaze df for the specified players.
-        When lookback or lookahead are provided, extends the selection window.
+    def get_start_end_times(self):
+        """Overrides this method to account for lookback and window_seconds.
         """
-        df = pd.read_csv(self.main_log_file, index_col="timestamp", parse_dates=["timestamp"])
-        df = df.sort_index()
-        start = self.segment_params['start'] - timedelta(seconds=lookback)
-        end = (
-            self.segment_params['start'] + 
-            timedelta(seconds=self.segment_params['duration']) + 
-            timedelta(seconds=lookahead)
-        )
-        df = df.loc[start:end]
-        lgdf = get_location_gaze(df, self.params['players'])
-        return lgdf
+        ws = self.params.get('window_seconds', self.default_window_seconds)
+        lookback = max(self.params.get('lookback', 0), ws)
+        start = self.segment_params['start'] - timedelta(lookback)
+        end = self.segment_params['start'] + timedelta(seconds=self.segment_params['duration'] + ws)
+        return start, end
 
     def trim_df(self, df):
         """Trims a df to start and end (start+duration) times defined in segment.
@@ -64,15 +60,14 @@ class SegmentJointAttention(SegmentLogs):
             raise ValueError("Unsupported measure: {}".format(self.params['measure']))
 
     def export_joint_attention_schneider_pea_2013(self):
-        default_ws = signature(joint_attention_schneider_pea_2013).parameters['window_seconds'].default
-        ws = self.params.get('window_seconds', default_ws)
-        lookback = max(self.params.get('lookback', 0), ws)
-        lookahead = ws
-        lgdf = self.get_location_gaze(lookback=lookback, lookahead=lookahead)
+        ws = self.params.get('window_seconds', self.default_window_seconds)
+        dt = self.params.get('distance_threshold', self.default_distance_threshold)
+        df = self.get_segment_data()
+        lgdf = get_location_gaze(df, self.params['players'])
         keypairs = list(combinations(self.params['players'].keys(), 2))
         for a, b in keypairs:
             lgdf[a + '-' + b] = joint_attention_schneider_pea_2013(lgdf, a, b, 
-                    distance_threshold=self.params.get('distance_threshold'), window_seconds=ws)
+                    distance_threshold=dt, window_seconds=ws)
         result = lgdf[[a + '-' + b for a, b in keypairs]]
         result = self.trim_df(result)
         result.to_csv(self.export_filename())
